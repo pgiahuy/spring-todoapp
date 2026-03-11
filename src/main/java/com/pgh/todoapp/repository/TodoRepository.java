66bc8df89a0120f890b981bd45d139a013c3c714
GiaHuy999 <huyphan2792005@gmail.com>
2026-03-11 22:18:42 +0700

    style

--- src/main/java/com/pgh/todoapp/repository/TodoRepository.java
+++ src/main/java/com/pgh/todoapp/repository/TodoRepository.java
@@ -2,6 +2,8 @@ package com.pgh.todoapp.repository;
 
 import com.pgh.todoapp.entity.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 
 public interface TodoRepository extends JpaRepository<TodoEntity,Long> {
 }
